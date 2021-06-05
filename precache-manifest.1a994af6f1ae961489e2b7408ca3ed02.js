74f2d109ed44a42945b39fb0e1d96683b62a74e9
Tim Johnson <tim.johnson1620@gmail.com>
2021-06-04 22:03:45 -0400

    Updates

---
+++ precache-manifest.1a994af6f1ae961489e2b7408ca3ed02.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363a2e305e639eff44f9efb0543df2fd",
+    "url": "/tajohnson16/MoneyManager/index.html"
+  },
+  {
+    "revision": "b7b10252edb60400ed1e",
+    "url": "/tajohnson16/MoneyManager/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9964577755e9b994a630",
+    "url": "/tajohnson16/MoneyManager/static/css/main.aa2b2c05.chunk.css"
+  },
+  {
+    "revision": "b7b10252edb60400ed1e",
+    "url": "/tajohnson16/MoneyManager/static/js/2.23fe1e1e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tajohnson16/MoneyManager/static/js/2.23fe1e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9964577755e9b994a630",
+    "url": "/tajohnson16/MoneyManager/static/js/main.4a12b03c.chunk.js"
+  },
+  {
+    "revision": "2477cfc145eb0771243e",
+    "url": "/tajohnson16/MoneyManager/static/js/runtime-main.8a503973.js"
+  },
+  {
+    "revision": "d93d3bd43209f3b636b62c3955c171d9",
+    "url": "/tajohnson16/MoneyManager/static/media/assessment.d93d3bd4.png"
+  },
+  {
+    "revision": "8c8083c9dcb34f242dbc9502ccd84134",
+    "url": "/tajohnson16/MoneyManager/static/media/results.8c8083c9.png"
+  }
+]);
