4c476ee13b54d47c74e7e9d25acb4ac2a52b8616
Tim Johnson <tim.johnson1620@gmail.com>
2021-06-05 21:56:23 -0400

    Fix text coloring and alignment

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -6,7 +6,7 @@ import Row from 'react-bootstrap/Row';
 function Jumbo(props) {
     return (
         <Jumbotron>
-            <Row className="justify-content-center py-2">
+            <Row className="justify-content-center text-center py-2">
                 <Col md={8} sm={12}>
                     <h1 className="display-3 font-weight-bolder">{props.title}</h1>
                     <h3 className="display-4 font-weight-light">{props.subTitle}</h3>
