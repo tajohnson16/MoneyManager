ba8b8d228da6c4870179a1f88a2bdd8623e1fdb8
Tim Johnson <tim.johnson1620@gmail.com>
2021-06-12 15:36:15 -0400

    ~ App.js from class to functional component

--- src/App.js
+++ src/App.js
@@ -17,65 +17,62 @@ import SpendingMoney from './FinancialQuestions/SpendingMoney';
 import Name from './FinancialQuestions/Name';
 import Profession from './FinancialQuestions/Profession';
 import money from './assets/money.png';
+import useSessionStorage from './components/SessionHook';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const App = props => {
+  const [spendPercent] = useSessionStorage('spendPercent');
+  const pageAssets = {
+    title: 'Real Money',
+    headerLinks: [
+      { title: 'Home', path: '/' },
+      { title: 'About', path: '/about' }
+    ],
+    home: {
       title: 'Real Money',
-      headerLinks: [
-        { title: 'Home', path: '/' },
-        { title: 'About', path: '/about' }
-      ],
-      home: {
-        title: 'Real Money',
-        subTitle: 'Financial Planning for New Grads'
-      },
-      about: {
-        title: 'About Real Money'
-      }
+      subTitle: 'Financial Planning for New Grads'
+    },
+    about: {
+      title: 'About Real Money'
     }
   }
 
-  render() {
-    return (
-      <Router>
-        <Container className="container" style={{ maxWidth: "1320px" }} fluid={true}>
-          <Navbar sticky="top" className="border-bottom" bg="white" expand="lg">
-            <Navbar.Brand><Nav.Link style={{ color: "#212529", textDecoration: 'none' }} href="/MoneyManager/">Real Money <img src={money} height="25px"></img></Nav.Link></Navbar.Brand>
-            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-            <Navbar.Collapse id="navbar-toggle">
-              <Nav defaultActiveKey="/MoneyManager/" variant="pills" className='ml-auto'>
-                <Nav.Item >
-                  <Nav.Link id="homeNav" href="/MoneyManager/">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link as={Link} eventKey="about" to="/MoneyManager/about">About</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+  return (
+    <Router>
+      <Container className="container" style={{ maxWidth: "1320px" }} fluid={true}>
+        <Navbar sticky="top" className="border-bottom" bg="white" expand="lg">
+          <Navbar.Brand><Nav.Link style={{ color: "#212529", textDecoration: 'none' }} href="/MoneyManager/">Real Money <img src={money} height="25px"></img></Nav.Link></Navbar.Brand>
+          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav defaultActiveKey="/MoneyManager/" variant="pills" className='ml-auto'>
+              <Nav.Item >
+                <Nav.Link id="homeNav" href="/MoneyManager/">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} eventKey="about" to="/MoneyManager/about">About</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-          <Route path="/MoneyManager/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} />} />
-          <Route path="/MoneyManager/about" exact render={() => <AboutPage title={this.state.about.title} />} />
-          <Route path="/MoneyManager/results" exact render={() => <ResultsPage />} />
-          <Route path="/MoneyManager/assessment" exact render={() => <AssessmentPage />} />
-          <Route path="/MoneyManager/salary" exact render={() => <Salary />} />
-          <Route path="/MoneyManager/house" exact render={() => <HousePercent />} />
-          <Route path="/MoneyManager/car" exact render={() => <CarPercent />} />
-          <Route path="/MoneyManager/spend" exact render={() => <SpendingMoney />} />
-          <Route path="/MoneyManager/name" exact render={() => <Name />} />
-          <Route path="/MoneyManager/profession" exact render={() => <Profession />} />
-
+        <Route path="/MoneyManager/" exact render={() => <HomePage title={pageAssets.home.title} subTitle={pageAssets.home.subTitle} />} />
+        <Route path="/MoneyManager/about" exact render={() => <AboutPage title={pageAssets.about.title} />} />
+        <Route path="/MoneyManager/results" exact render={() => <ResultsPage />} />
+        <Route path="/MoneyManager/assessment" exact render={() => <AssessmentPage />} />
+        <Route path="/MoneyManager/salary" exact render={() => <Salary />} />
+        <Route path="/MoneyManager/house" exact render={() => <HousePercent />} />
+        <Route path="/MoneyManager/car" exact render={() => <CarPercent />} />
+        <Route path="/MoneyManager/spend" exact render={() => <SpendingMoney />} />
+        <Route path="/MoneyManager/name" exact render={() => <Name />} />
+        <Route path="/MoneyManager/profession" exact render={() => <Profession />} />
 
 
 
 
-          <Footer />
-        </Container>
-      </Router>
-    );
-  }
+
+        <Footer />
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
